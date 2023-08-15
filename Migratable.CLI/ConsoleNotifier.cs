8e662a86313734d7dcf3ee3cff5549215d78bdb2
K Cartlidge <code@kcartlidge.com>
2023-08-15 07:41:41 +0100

    Add Migratable.CLI console app

---
+++ Migratable.CLI/ConsoleNotifier.cs
@@ -0,0 +1,14 @@
+﻿using Migratable.Interfaces;
+using Migratable.Models;
+
+namespace Migratable.CLI
+{
+    internal class ConsoleNotifier : INotifier
+    {
+        public void Notify(Migration migration, Direction direction)
+        {
+            Console.WriteLine($"{direction.ToString().ToUpper()} {migration.Version}:{migration.Name}");
+        }
+    }
+}
+
